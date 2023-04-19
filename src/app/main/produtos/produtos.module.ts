7fef536248fab85178226013898073f0bf450e89
Rossetti <mh.rossetti2002@gmail.com>
2023-04-19 16:44:38 -0300

    Adicionado tabelas

--- src/app/main/produtos/produtos.module.ts
+++ src/app/main/produtos/produtos.module.ts
@@ -3,17 +3,29 @@ import {CommonModule} from '@angular/common';
 
 import {ProdutosRoutingModule} from './produtos-routing.module';
 import {ProdutosListComponent} from './produtos-list/produtos-list.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
     ProdutosListComponent
   ],
-  exports: [
-    ProdutosListComponent
-  ],
   imports: [
     CommonModule,
-    ProdutosRoutingModule
+    ProdutosRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
   ]
 })
 export class ProdutosModule {
